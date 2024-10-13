2f1914a22906f0bf631bf37c67734d60565a63df
GuoJiaqi <2667629684@qq.com>
2024-10-13 20:36:46 +0800

    学习vueuse组件库

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,11 @@ import './style.css'
 // import E2 from './views/E2.vue'
 // import E3 from './views/E3.vue'
 // import E4 from './views/E4.vue'
-import E5 from './views/E5.vue'
+// import E5 from './views/E5.vue'
 // import E6 from './views/E6.vue'
+import E7 from './views/E7.vue'
 
 // createApp(E6).mount('#app')
-createApp(E5).mount('#app')
+createApp(E7).mount('#app')
 
 // console.log(import.meta.env.VITE_BACKEND_API_BASE_URL)
---
+++ src/views/E7.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+import {useCounter, useMouse} from "@vueuse/core";
+
+const {x, y} = useMouse();
+
+const {count, inc, dec} = useCounter();
+
+</script>
+
+<template>
+
+	<h3>x:{{ x }}</h3>
+	<h3>y:{{ y }}</h3>
+
+	<h3>{{ count }}</h3>
+	<button type="button" @click="inc()">+</button>
+	<button type="button" @click="dec()">-</button>
+</template>
+
+<style scoped>
+
+</style>
